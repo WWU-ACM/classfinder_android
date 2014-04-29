402bcbaf69ea61d80f95562ee6d805bf00dcfb1b
Xyresic <zyresic@gmail.com>
2014-04-29 04:56:07 -0700

    progress for checkpoint 1

--- WWUClassfinder2/src/edu/wwu/classfinder2/FragmentTranscript.java
+++ WWUClassfinder2/src/edu/wwu/classfinder2/FragmentTranscript.java
@@ -7,64 +7,112 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.OutputStream;
 import java.net.URL;
-import java.net.HttpURLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class FragmentTranscript extends Fragment {
-	
+
 	class TranscriptTask extends AsyncTask<URL, Integer, String> {
 
 		@Override
 		protected String doInBackground(URL... params) {
-			
 			try {
 				URL url = params[0];
-				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
 				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 				return IOUtils.toString(in);
-			} catch (Exception e){
-				e.printStackTrace();
+			} catch (Exception e) {
 				return "Something went wrong in AsyncTask";
 			}
 		}
 	}
-	
+
+	class LoginTask extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+
+			try {				
+				Document doc = Jsoup.connect(params[0]).get();
+				// <!-- The following hidden fields must be part of the submitted Form -->
+				Elements hidden = doc.select("input[type=hidden]");
+				
+				URL url = new URL(params[0]);
+				HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+				conn.setRequestMethod("POST");
+				conn.setConnectTimeout(5000);
+				conn.setReadTimeout(5000);
+				conn.setDoOutput(true);
+				conn.setDoInput(true);
+				
+				OutputStream out = conn.getOutputStream();
+				
+				return null;
+			} catch (IOException e) {
+				return "Something went wrong";
+			}
+		}
+	}
+
 	@Override
-	public void onAttach(Activity activity){
+	public void onAttach(Activity activity) {
 		super.onAttach(activity);
 	}
-	
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup contrainer, Bundle savedInstanceState){
-		View view = inflater.inflate(R.layout.fragment_main, null);
-		
-		URL url = null;
-		try { url = new URL("https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran"); }
-		catch (MalformedURLException e) { e.printStackTrace(); }
-		
+	public View onCreateView(LayoutInflater inflater, ViewGroup contrainer, Bundle savedInstanceState) {
+		final View view = inflater.inflate(R.layout.fragment_main, null);
+
+		Button mLogin = (Button) view.findViewById(R.id.login);
+		mLogin.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String username = ((TextView) view.findViewById(R.id.username)).getText().toString();
+				String password = ((TextView) view.findViewById(R.id.password)).getText().toString();
+
+				TextView page = (TextView) view.findViewById(R.id.page);
+				try {
+					page.setText(login(username, password));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		});
+
+		return view;
+	}
+
+	private String login(String username, String password) throws Exception {
+		URL url = new URL("https://websso.wwu.edu/cas/login");
+		LoginTask lt = new LoginTask();
+		lt.execute("https://websso.wwu.edu/cas/login");
+		return lt.get();
+	}
+
+	private String getTranscript(String username, String password) throws Exception {
+		URL url = new URL("https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran");
+
 		TranscriptTask mtt = new TranscriptTask();
 		mtt.execute(url);
-		
-		String in = null;		
-		try {
-			in = mtt.get();
-			Log.d("HI", in);
-		} catch (Exception e){
-			e.printStackTrace();
-			Log.d("HI", "Something went wrong");
-		}
-		
-		TextView page = (TextView) view.findViewById(R.id.page);
-		page.setText(in);
-		
-		return view;
+		return mtt.get();
 	}
 }
