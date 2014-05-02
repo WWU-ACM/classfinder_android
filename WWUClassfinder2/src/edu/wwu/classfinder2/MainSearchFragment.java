3639d9410e1d4986229f2fe431de60ffba645a6f
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-01 21:27:35 -0700

    Add basic search view fragment and layout

---
+++ WWUClassfinder2/src/edu/wwu/classfinder2/MainSearchFragment.java
@@ -0,0 +1,36 @@
+package edu.wwu.classfinder2;
+
+import android.app.ListFragment;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ArrayAdapter;
+
+public class MainSearchFragment extends ListFragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (container == null)
+            return null;
+
+        View rootView = inflater.inflate(R.layout.fragment_search_main,
+                                         container,
+                                         false);
+
+        String[] values = new String[] { "Message1",
+                                         "Message2",
+                                         "Message3" };
+        ArrayAdapter<String> adapter =
+            new ArrayAdapter<String>(getActivity(),
+                                     android.R.layout.simple_list_item_1,
+                                     values);
+        setListAdapter(adapter);
+        return rootView;
+    }
+
+}
