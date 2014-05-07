a2eddbfaa71c20a7cc7ce37bfc6c57953ee1fcd5
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-06 17:36:18 -0700

    Display transcript fragment in main activity

--- WWUClassfinder2/src/edu/wwu/classfinder2/ActivityMain.java
+++ WWUClassfinder2/src/edu/wwu/classfinder2/ActivityMain.java
@@ -1,6 +1,8 @@
 package edu.wwu.classfinder2;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,6 +12,13 @@ public class ActivityMain extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        TranscriptFragment transcriptFrag = new TranscriptFragment();
+        ft.add(R.id.fragment_container, transcriptFrag);
+        ft.commit();
     }
 
 }
