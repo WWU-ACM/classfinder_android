0c7d25380f56d11dde2036998fb33b96c612df2d
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-06 17:22:46 -0700

    Move transcript loading code into fragment
    Not quite working yet, but basically there.  Compiles and runs without
    errors.

--- WWUClassfinder2/src/edu/wwu/classfinder2/ActivityMain.java
+++ WWUClassfinder2/src/edu/wwu/classfinder2/ActivityMain.java
@@ -1,57 +1,15 @@
 package edu.wwu.classfinder2;
 
 import android.app.Activity;
-import android.app.Fragment;
 
 import android.os.Bundle;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.webkit.ValueCallback;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import android.widget.Toast;
-
-public class ActivityMain
-    extends Activity implements ValueCallback<String> {
-
-    private static String TRANSCRIPT_URL =
-        "https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran";
+public class ActivityMain extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final ValueCallback<String> mActivity = this;
-
-        final WebView mWebView =
-            (WebView) findViewById(R.id.activity_main_webview);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        mWebView.setWebViewClient(new WebViewClient() {
-                @Override
-                public void onPageFinished(WebView view, String url) {
-                    if (url.equals(TRANSCRIPT_URL)) {
-                        mWebView.evaluateJavascript("document.getElementsByTagName('html')[0].innerHTML", mActivity);
-                    }
-                }
-            });
-
-        mWebView.loadUrl(TRANSCRIPT_URL);
-    }
-
-    public void onReceiveValue(String value) {
-        if (value != null) {
-            Toast.makeText(this, value,
-                           Toast.LENGTH_SHORT).show();
-
-        }
     }
 
 }
---
+++ WWUClassfinder2/src/edu/wwu/classfinder2/TranscriptFragment.java
@@ -0,0 +1,66 @@
+package edu.wwu.classfinder2;
+
+import android.app.Fragment;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.widget.Toast;
+
+public class TranscriptFragment
+    extends Fragment
+    implements ValueCallback<String> {
+
+    private static String TRANSCRIPT_URL =
+        "https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (container == null)
+            return null;
+        return inflater.inflate(R.layout.fragment_load_transcript,
+                                container,
+                                false);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedState) {
+        super.onActivityCreated(savedState);
+
+        final ValueCallback<String> mActivity = this;
+
+        final WebView mWebView = (WebView) getView();
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    if (url.equals(TRANSCRIPT_URL)) {
+                        mWebView.evaluateJavascript("document.getElementsByTagName('html')[0].innerHTML", mActivity);
+                    }
+                }
+            });
+
+        mWebView.loadUrl(TRANSCRIPT_URL);
+    }
+
+    public void onReceiveValue(String value) {
+        if (value != null) {
+            Toast.makeText(getActivity(), value,
+                           Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+}
