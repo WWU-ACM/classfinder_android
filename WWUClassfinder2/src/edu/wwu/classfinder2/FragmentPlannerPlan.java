ec4a088e75747cdc8a58feca8636fb42d9ed6604
Xyresic <zyresic@gmail.com>
2014-05-12 05:38:48 -0700

    screw FragmentPagerAdapters... going with simple fragments instead

--- WWUClassfinder2/src/edu/wwu/classfinder2/FragmentPlannerPlan.java
+++ WWUClassfinder2/src/edu/wwu/classfinder2/FragmentPlannerPlan.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentPlannerPlan extends Fragment {
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup contrainer, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_planner_plan, null);
