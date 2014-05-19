4e51f8f2788b3a09ee731f9347c7609beab0e60f
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-19 13:50:30 -0700

    Switch to the gradle build system
    Sans tests

---
+++ WWUClassfinder2/src/test/java/edu/wwu/classfinder2/test/activity/WWUClassfinderActivityTest.java
@@ -0,0 +1,22 @@
+package edu.wwu.classfinder2.test.activity;
+
+import android.app.Activity;
+
+import edu.wwu.classfinder2.ActivityMain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class WWUClassfinderActivityTest {
+
+    @Test
+    public void testSomething() throws Exception {
+    }
+}
