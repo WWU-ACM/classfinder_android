41a494284330e45587a6e226ea0ea9866bd4c0cf
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-18 21:17:21 -0700

    Add test for ScheduleParser

---
+++ WWUClassfinder2/tests/src/edu/wwu/classfinder2/test/data/ScheduleParserTest.java
@@ -0,0 +1,36 @@
+package edu.wwu.classfinder2.test.data;
+
+import android.support.v4.util.ArrayMap;
+
+import edu.wwu.classfinder2.data.ScheduleParser;
+import edu.wwu.classfinder2.data.Schedule.Meeting;
+
+import java.util.Map;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.threeten.bp.DayOfWeek;
+import org.threeten.bp.Duration;
+import org.threeten.bp.LocalTime;
+
+import org.threeten.bp.temporal.ChronoUnit;
+
+public class ScheduleParserTest extends TestCase {
+
+    public void testParse() {
+        Map<String, Meeting> cases = new ArrayMap<String, Meeting>();
+        cases.put("M 10:00-10:50 am",
+                  new Meeting(DayOfWeek.MONDAY,
+                              LocalTime.of(10,0),
+                              Duration.of(50, ChronoUnit.MINUTES)));
+
+        for (Map.Entry<String, Meeting> entry : cases.entrySet()) {
+            ScheduleParser sp = new ScheduleParser(entry.getKey());
+            Assert.assertEquals("Meetings should parse equivalently",
+                                entry.getValue(),
+                                sp.iterator().next());
+        }
+    }
+
+}
