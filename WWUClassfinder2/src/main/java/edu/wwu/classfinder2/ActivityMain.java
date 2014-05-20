cf7751548a49c15246f11aaf2c6d8abb6ddde5e1
Xyresic <zyresic@gmail.com>
2014-05-20 04:18:31 -0700

    rotation fix for actionbar titles

--- WWUClassfinder2/src/main/java/edu/wwu/classfinder2/ActivityMain.java
+++ WWUClassfinder2/src/main/java/edu/wwu/classfinder2/ActivityMain.java
@@ -3,27 +3,22 @@ package edu.wwu.classfinder2;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
-
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -35,7 +30,7 @@ public class ActivityMain extends ActionBarActivity {
     // Constants
     // The authority for the sync adapter's content provider
     public static final String AUTHORITY =
-        "edu.wwu.classfinder2.provider";
+            "edu.wwu.classfinder2.provider";
     // An account type, in the form of a domain name
     // The account name
     public static final String ACCOUNT = "dummyaccount";
@@ -47,6 +42,7 @@ public class ActivityMain extends ActionBarActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private Account mAccount;
     private ContentResolver mResolver;
+    private static final String[] TAG = {"FRAG_TRANSCRIPT", "FRAG_SEARCH", "FRAG_PLANNER", "FRAG_CALENDAR", "FRAG_BEGIN"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +51,10 @@ public class ActivityMain extends ActionBarActivity {
 
         // Add the dummy account to the Sync service
         mAccount =
-            CreateCourseSyncAccount(this.getApplicationContext(),
-                                    getResources()
-                                    .getString(R.string.account_type));
+                CreateCourseSyncAccount(this.getApplicationContext(),
+                        getResources()
+                                .getString(R.string.account_type)
+                );
 
         mResolver = getContentResolver();
         if (mResolver != null) {
@@ -68,18 +65,19 @@ public class ActivityMain extends ActionBarActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.drawer_menu);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.drawer_navigation,
-                                                  R.string.drawer_open, R.string.drawer_close) {
-                @Override
-                public void onDrawerOpened(View drawerView) {
-                    super.onDrawerOpened(drawerView);
-                    getSupportActionBar().setTitle(R.string.app_name);
-                }
-                @Override
-                public void onDrawerClosed(View drawerView){
-                    super.onDrawerClosed(drawerView);
-                    getSupportActionBar().setTitle(mTitle);
-                }
-            };
+                R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.app_name);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mTitle);
+            }
+        };
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -87,14 +85,17 @@ public class ActivityMain extends ActionBarActivity {
 
         // Set adapter for list view
         mDrawerList.setAdapter(new DrawerMenuAdapter(this));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener(this));
 
         // Reset title on configuration change
-        if (savedInstanceState != null && savedInstanceState.getString("ActionBarTitle") != null) {
-            setTitle(savedInstanceState.getString("ActionBarTitle"));
+        if (savedInstanceState != null && savedInstanceState.getString("MTITLE") != null) {
+            mTitle = savedInstanceState.getString("MTITLE");
+            if (savedInstanceState.getBoolean("DRAWER_OPEN") != true){
+                getSupportActionBar().setTitle(mTitle);
+            }
         }
 
-        // Open the drawer on startup
+        // Open the drawer only on startup
         if (savedInstanceState == null) {
             mDrawerLayout.openDrawer(mDrawerList);
             getFragmentManager().beginTransaction().add(R.id.drawer_content, new FragmentStart()).commit();
@@ -103,7 +104,8 @@ public class ActivityMain extends ActionBarActivity {
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        outState.putString("ActionBarTitle", getSupportActionBar().getTitle().toString());
+        outState.putString("MTITLE", mTitle);
+        outState.putBoolean("DRAWER_OPEN", mDrawerLayout.isDrawerOpen(mDrawerList));
         super.onSaveInstanceState(outState);
     }
 
@@ -136,11 +138,11 @@ public class ActivityMain extends ActionBarActivity {
     public static Account CreateCourseSyncAccount(Context context, String accountType) {
         // Create the account type and default account
         Account newAccount = new Account(ACCOUNT,
-                                         accountType);
+                accountType);
 
         // Get an instance of the Android account manager
         AccountManager accountManager =
-            (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
+                (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
         /*
          * Add the account and account type, no password or user data
          * If successful, return the Account object, otherwise report
@@ -155,7 +157,7 @@ public class ActivityMain extends ActionBarActivity {
              * accounts for our account_type.
              */
             Account[] accounts =
-                accountManager.getAccountsByType(accountType);
+                    accountManager.getAccountsByType(accountType);
             if (accounts.length != 0) {
                 return accounts[0];
             } else {
@@ -216,31 +218,43 @@ public class ActivityMain extends ActionBarActivity {
     }
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        Activity activity;
+
+        public DrawerItemClickListener(Activity activity){
+            this.activity = activity;
+        }
+
         /* Swaps fragments in the main content view */
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            // Hide the keyboard
+            InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+
             Fragment fragment;
 
             switch (position) {
-            case 0:
-                fragment = new FragmentTranscript();
-                setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[0]);
-                break;
-            case 1:
-                fragment = new FragmentSearch();
-                setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[1]);
-                break;
-            case 2:
-                fragment = new FragmentPlanner();
-                setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[2]);
-                break;
-            case 3:
-                fragment = new Fragment();
-                setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[3]);
-                break;
-            default:
-                fragment = null;
-                break;
+                case 0:
+                    fragment = new FragmentTranscript();
+                    setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[0]);
+                    break;
+                case 1:
+                    fragment = new FragmentSearch();
+                    setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[1]);
+                    break;
+                case 2:
+                    fragment = new FragmentPlanner();
+                    setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[2]);
+                    break;
+                case 3:
+                    fragment = new Fragment();
+                    setTitle(getResources().getStringArray(R.array.drawer_menu_titles)[3]);
+                    break;
+                default:
+                    fragment = null;
+                    break;
             }
 
             FragmentManager fm = getFragmentManager();
